e8520be46ecab275c4dbdf41d3fa20ba918f4c2f
Rahul R Singh <60823960+rahulsingh97@users.noreply.github.com>
2020-09-16 22:43:28 +0530

    Create car basic controls.ino

---
+++ car basic controls.ino
@@ -0,0 +1,61 @@
+
+#include <IRremote.h>
+const int RECV_PIN = 8;
+unsigned long lastCode; 
+IRrecv irrecv(RECV_PIN);
+decode_results results;
+void setup(){Serial.begin(9600);irrecv.enableIRIn();
+pinMode(4,OUTPUT);pinMode(2,OUTPUT);
+digitalWrite(4,HIGH);digitalWrite(2,HIGH);
+pinMode(3,OUTPUT);pinMode(5,OUTPUT);//weel
+pinMode(6,OUTPUT);pinMode(9,OUTPUT);//weel
+pinMode(10,OUTPUT);pinMode(11,OUTPUT);//turn
+}
+void loop() {
+if(irrecv.decode(&results)){
+    if(results.value == 4294967295){results.value = lastCode;}
+    if(results.value == 16422){lastCode = results.value;
+    Serial.println("forword");
+    digitalWrite(3,HIGH);digitalWrite(5,LOW);
+    digitalWrite(6,HIGH);digitalWrite(9,LOW);
+    delay(10);
+    digitalWrite(3,LOW);digitalWrite(5,LOW);
+    digitalWrite(6,LOW);digitalWrite(9,LOW);
+        //function                     
+    }
+    if(results.value == 24614){lastCode = results.value;
+    Serial.println("left");
+    digitalWrite(3,HIGH);digitalWrite(5,LOW);
+    digitalWrite(6,HIGH);digitalWrite(9,LOW);
+    digitalWrite(10,HIGH);digitalWrite(11,LOW);
+    delay(10);
+    digitalWrite(3,LOW);digitalWrite(5,LOW);
+    digitalWrite(6,LOW);digitalWrite(9,LOW);
+    digitalWrite(10,LOW);digitalWrite(11,LOW);
+       //function     
+    }
+    if(results.value == 20518){lastCode = results.value;
+    Serial.println("right");
+    digitalWrite(3,HIGH);digitalWrite(5,LOW);
+    digitalWrite(6,HIGH);digitalWrite(9,LOW);
+    digitalWrite(10,LOW);digitalWrite(11,HIGH);
+    delay(10);
+    digitalWrite(3,LOW);digitalWrite(5,LOW);
+    digitalWrite(6,LOW);digitalWrite(9,LOW);
+    digitalWrite(10,LOW);digitalWrite(11,LOW);
+       //function     
+    }
+    if(results.value == 28710){lastCode = results.value;
+    Serial.println("back");
+    digitalWrite(3,LOW);digitalWrite(5,HIGH);
+    digitalWrite(6,LOW);digitalWrite(9,HIGH);
+    delay(10);
+    digitalWrite(3,LOW);digitalWrite(5,LOW);
+    digitalWrite(6,LOW);digitalWrite(9,LOW);
+       //function     
+    }
+    //Serial.println(lastCode);
+    //Serial.println(results.value);
+    delay(90); 
+    irrecv.resume(); 
+}}
